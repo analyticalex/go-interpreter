039662afda878b9b880b1f76d5fa8ee3efaf8ef3
Alex French <alexandra.j.french@gmail.com>
2017-02-23 13:38:36 -0800

    Correcting method capitalization

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -20,12 +20,12 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
-	l.ReadChar()
+	l.readChar()
 	return l
 }
 
 // Read current character, increments position and readPosition
-func (l *Lexer) ReadChar() {
+func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
 	} else {
@@ -41,34 +41,36 @@ func (l *Lexer) NextToken() token.Token {
 	
 	switch l.ch {
 	case '=':
-		tok = NewToken(token.ASSIGN, l.ch)
+		tok = newToken(token.ASSIGN, l.ch)
 	case '+':
-		tok = NewToken(token.PLUS, l.ch)
+		tok = newToken(token.PLUS, l.ch)
 	case ',':
-		tok = NewToken(token.COMMA, l.ch)
+		tok = newToken(token.COMMA, l.ch)
 	case ';':
-		tok = NewToken(token.SEMICOLON, l.ch)
+		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
-		tok = NewToken(token.LPAREN, l.ch)
+		tok = newToken(token.LPAREN, l.ch)
 	case ')':
-		tok = NewToken(token.RPAREN, l.ch)
+		tok = newToken(token.RPAREN, l.ch)
 	case '{':
-		tok = NewToken(token.LBRACE, l.ch)
+		tok = newToken(token.LBRACE, l.ch)
 	case '}':
-		tok = NewToken(token.RBRACE, l.ch)
+		tok = newToken(token.RBRACE, l.ch)
 	case '[':
-		tok = NewToken(token.LBRACK, l.ch)
+		tok = newToken(token.LBRACK, l.ch)
 	case ']':
-		tok = NewToken(token.RBRACK, l.ch)
+		tok = newToken(token.RBRACK, l.ch)
 	case 0:
 		tok.Type = token.EOF
-		tok.Literal = ""	
+		tok.Literal = ""
+	default:
+		if isLetter(l.ch)
 	}
 	
-	l.ReadChar()
+	l.readChar()
 	return tok
 }
 
-func NewToken(tokenType token.TokenType, ch byte) token.Token {
+func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
